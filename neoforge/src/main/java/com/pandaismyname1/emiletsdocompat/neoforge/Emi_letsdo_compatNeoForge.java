8a68712f2995460524296e49e931fd463b62d789
panda <pandaismyname1@gmail.com>
2025-10-14 04:04:04 +0300

    Refactored the project to support `runClient` configurations.

--- neoforge/src/main/java/com/pandaismyname1/emiletsdocompat/neoforge/Emi_letsdo_compatNeoForge.java
+++ neoforge/src/main/java/com/pandaismyname1/emiletsdocompat/neoforge/Emi_letsdo_compatNeoForge.java
@@ -2,11 +2,12 @@ package com.pandaismyname1.emiletsdocompat.neoforge;
 
 import com.pandaismyname1.emiletsdocompat.Emi_letsdo_compat;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
 @Mod(Emi_letsdo_compat.MOD_ID)
-public class Emi_letsdo_compatNeoForge {
-    public Emi_letsdo_compatNeoForge(IEventBus bus) {
+public final class Emi_letsdo_compatNeoForge {
+    public Emi_letsdo_compatNeoForge(IEventBus bus, ModContainer modContainer) {
         Emi_letsdo_compat.init();
     }
 }
